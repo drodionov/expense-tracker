f84eed820de87c401ffbce069863ca11b3002e34
Dmitrii Rodionov <drodionov.work@gmail.com>
2023-02-14 00:32:15 +0300

    Implement the very first component! Easy-peasy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem"
+
 function App() {
   return (
       <div>
         <h2>Brand new app!</h2>
+        <ExpenseItem/>
       </div>
-  );
+  )
 }
 
-export default App;
+export default App
