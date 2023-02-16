a2690863fca499f58539988f8c371f7a17ff4ca3
Dmitrii Rodionov <sf.zeon@gmail.com>
2023-02-16 22:55:20 +0300

    Add some styles for ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
-  return <h2>Expense Item!</h2>
+  return (<div className="expense-item">
+    <div>February 17th 2023</div>
+    <div className={"expense-item__description"}>
+      <h2>Cheese</h2>
+      <div className={"expense-item__price"}>$5.55</div>
+    </div>
+  </div>)
 }
 
 export default ExpenseItem
