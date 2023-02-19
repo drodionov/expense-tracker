84afe8200f8c270da2e96994860fba438b1de30e
Dmitrii Rodionov <sf.zeon@gmail.com>
2023-02-19 18:30:50 +0300

    Create a Card common wrapper

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,14 +1,15 @@
 import './Expenses.css'
 import ExpenseItem from "./ExpenseItem"
+import Card from "./Card";
 
 function Expenses(props) {
   const expenseItems = props.items.map(
       (props) => <ExpenseItem className={'expense-item'} date={props.date} title={props.title}
                                   amount={props.amount}/>)
   return (
-      <div className={'expenses'}>
+      <Card className={'expenses'}>
         {expenseItems}
-      </div>
+      </Card>
   )
 }
 
