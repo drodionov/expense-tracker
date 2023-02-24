fc3192dea122f876cd987c37de382b1cb1711978
Dmitrii Rodionov <sf.zeon@gmail.com>
2023-02-24 18:34:56 +0300

    Move stub to global constant

--- src/App.js
+++ src/App.js
@@ -2,24 +2,24 @@ import Expenses from "./components/Expenses/Expenses"
 import NewExpense from "./components/NewExpense/NewExpense";
 import {useState} from "react"
 
+const DUMMY_EXPENSES = [
+  {id: 1, date: new Date(2023, 1, 16), title: 'XBOX One S', amount: 600},
+  {id: 2, date: new Date(2023, 1, 15), title: 'XBOX One X', amount: 1000},
+  {
+    id: 3,
+    date: new Date(2023, 1, 14),
+    title: 'Sony Playstation 5',
+    amount: 950
+  },
+  {id: 4, date: new Date(2023, 1, 13), title: 'Nintendo Switch', amount: 400},
+  {id: 5, date: new Date(2023, 1, 13), title: 'SteamDeck', amount: 499.99}
+];
 const App = () => {
-  const [expenseData, expenseDataSet] = useState([
-    {id: 1, date: new Date(2023, 1, 16), title: 'XBOX One S', amount: 600},
-    {id: 2, date: new Date(2023, 1, 15), title: 'XBOX One X', amount: 1000},
-    {
-      id: 3,
-      date: new Date(2023, 1, 14),
-      title: 'Sony Playstation 5',
-      amount: 950
-    },
-    {id: 4, date: new Date(2023, 1, 13), title: 'Nintendo Switch', amount: 400},
-    {id: 5, date: new Date(2023, 1, 13), title: 'SteamDeck', amount: 499.99}
-  ])
+  const [expenseData, expenseDataSet] = useState(DUMMY_EXPENSES)
 
-  const addExpenseDataHandler = (expense) => {
-    console.log(expense)
+  const addExpenseDataHandler = expense => {
     expenseDataSet((prevState) => {
-      return [...prevState, expense]
+      return [expense, ...prevState]
     })
   }
 
